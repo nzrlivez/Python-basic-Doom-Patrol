4915a75edb53241cf7dea25c24baa61f65d78fb0
Nazar Danyliv <nzrlivez@gmail.com>
2021-12-12 23:42:18 +0200

    HW4_Danyliv Nazar(vr2)

--- homeworks/home work nd/hw_5.py
+++ homeworks/home work nd/hw_5.py
@@ -27,27 +27,109 @@
 # accord = accord()
 # guitar = guitar(accord)
 
-# 3.
-# class Calc :
-#     def instance_method(self):
-#         print(f'Called instance_method of {self}')
+
+# 3
+
+# class Calc:
+#     @staticmethod
+#     def add_nums(a, b, c):
+#         return a + b + c
+#
+#
+# m = Calc()
+# print(m.add_nums(5, 6, 7))
+
+
+# 4
+
+# class Pasta :
+#     def __init__(self, ingredients):
+#         self.ingredients = [ingredients]
+#
+#     def __repr__(self):
+#         return f'It consists of {self.ingredients}'
+#
+#     @classmethod
+#     def carbonara(cls):
+#         return cls(['forcemeat', 'tomatoes'])
 #
 #
 #     @classmethod
-#     def class_method(cls):
-#         print(f'Called class_method of {cls}')
+#     def bolognaise(cls):
+#         return cls(['bacon', 'parmesan', 'eggs'])
 #
-#
-#     @staticmethod
-#     def static_method():
-#         print('Called static_method')
+# pasta_1 = Pasta.carbonara()
+# pasta_2 = Pasta.bolognaise()
+# print(pasta_1)
+# print(pasta_2)
 
-# calcc = Calc()
-# calc.instance_method()
-# Calc.instance_method(calcc)
+ # 5.
+
+# class Concert :
+#     max_visitors_num = 0
 #
-# Calc.class_method()
-# Calc.static_method()
+#     def __init__(self):
+#         self._visitors_count = 0
+#
+#     @property
+#     def visitors_count(self):
+#         return self._visitors_count
+#
+#     @visitors_count.setter
+#     def visitors_count(self, num):
+#         if num < self.max_visitors_num:
+#             self._visitors_count = num
+#         else:
+#             self._visitors_count = self.max_visitors_num
+#
+#
+# Concert.max_visitors_num = 50
+# concert = Concert()
+# concert.visitors_count = 1000
+# print(concert.visitors_count)
+
+# 6.
+# import dataclasses
+#
+# @dataclasses.dataclass
+# class AddressBookDataClass :
+#     key : int
+#     name : str
+#     phone_number : str
+#     address : str
+#     email : str
+#     birthday : str
+#     age : int
+#
+# my_address = AddressBookDataClass (key=55,
+#                                name= 'Nazar',
+#                                phone_number= '998-889-999',
+#                                address= 'centralna',
+#                                email= 'nazar@gmail.com',
+#                                birthday= '15th March',
+#                                age=23)
+# print(my_address)
+
+
+# 7.
+# from collections import namedtuple
+#
+#
+# AddressBookDataClass_1 = namedtuple('AddressBookDataClass',
+#                                     ['key', 'name',
+#                                      'phone_number',
+#                                      'address', 'email',
+#                                      'birthday', 'age'])
+#
+# me_add = AddressBookDataClass_1(33, 'Nazar',
+#                                     '998-889-999',
+#                                     'centralna',
+#                                     'nazar@gmail.com',
+#                                     '15th March', 23)
+
+# 8.
+
+
 
 
 
