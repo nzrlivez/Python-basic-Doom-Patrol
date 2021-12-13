413c6839b033b9743f208bb91eeadc39c78311bd
Nazar Danyliv <nzrlivez@gmail.com>
2021-12-13 19:41:57 +0200

    HW4_Danyliv Nazar(vr2)

--- homeworks/home work nd/hw_5.py
+++ homeworks/home work nd/hw_5.py
@@ -121,17 +121,77 @@
 #                                      'address', 'email',
 #                                      'birthday', 'age'])
 #
-# me_add = AddressBookDataClass_1(33, 'Nazar',
+# my_address2 = AddressBookDataClass_1(33, 'Nazar',
 #                                     '998-889-999',
 #                                     'centralna',
 #                                     'nazar@gmail.com',
 #                                     '15th March', 23)
+# print(my_address2)
 
 # 8.
+# class AddressBook:
+#     def __init__(self, key, name,
+#                  phone_number, address,
+#                  email, birthday, age):
+#         self.key = int(key)
+#         self.name = str(name)
+#         self.phone_number = str(phone_number)
+#         self.address = str(address)
+#         self.email = str(email)
+#         self.birthday = str(birthday)
+#         self.age = int(age)
+#
+#     def __str__(self):
+#         return f'AddressBook {self.key},name = {self.name},' \
+#                f'phone_number = {self.phone_number}, address = {self.address},' \
+#                f'email = {self.email}, birthday = {self.birthday},' \
+#                f'age = {self.age}'
+#
+#
+# cursor_address = AddressBook(key=55, name='cursor',
+#                      phone_number='555-555-555',
+#                      address='lviv',
+#                      email='cursor@lviv',
+#                      birthday='1th September',
+#                      age=15)
+# print(cursor_address)
 
+# 9.
+# class Person:
+#     def __init__(self, name, age, country):
+#         self.name = name
+#         self.age = age
+#         self.country = country
+#
+#     def set_age(self, value):
+#         self.age = value
+#
+#
+# person_1 = Person("John", 36, "USA")
+# person_1.set_age(55)
+# print(person_1.age)
 
+# 10
+# class Student :
+#     def __init__(self, id, name):
+#         self.id = id
+#         self.name = name
+#
+# student = Student(0, 'Jack')
+#
+# setattr(student, 'student_email', 'jack@gmail.com')
+# print(getattr(student, 'student_email'))
 
-
-
-
-
+# 11.
+# class Celsius:
+#     def __init__(self, temperature=0):
+#         self._temperature = temperature
+#
+#
+#     @property
+#     def print_temp(self):
+#         return self._temperature * 1.8 + 32
+#
+# fahrenheit = Celsius(18)
+# fahrenheit._temperature = (fahrenheit._temperature * 1.8) + 32
+# print(fahrenheit._temperature)
