5a8834ef07a60acad82881e6f103569e7d16af8b
Nazar Danyliv <nzrlivez@gmail.com>
2021-12-24 18:01:43 +0200

    HW8_Danyliv

--- homeworks/home work nd/hw_8/hw_8.py
+++ homeworks/home work nd/hw_8/hw_8.py
@@ -1,12 +1,32 @@
-# while True:
-#     print("1.Add number1"
-#           + '2.Add number2\n'
-#             )
 
-print("User Input:\n")
-a=str(input(" "))
-c=str(input(" ")) #введення знаку арифметичної операції
-b=str(input(" "))
-d=a+c+b
-p=eval(d)
+while True:
+    i = input('Choose the operation : ' )
+    if i in ('+', '-', '*', '/'):
+        x = float(input('Write first number: '))
+        y = float(input('Write second number: '))
+        if i == '+':
+            addition = x + y
+            print(addition)
+            with open('result.txt', 'a') as file:
+                file.write(str(f'{x} {i} {y} = {addition}' + '\n'))
+        elif i == '-':
+            subtraction = x - y
+            print(subtraction)
+            with open('result.txt', 'a') as file:
+                file.write(str(f'{x} {i} {y} = {subtraction}' + '\n'))
+        elif i == '*':
+            multiplication = x * y
+            print(multiplication)
+            with open('result.txt', 'a') as file:
+                file.write(str(f'{x} {i} {y} = {multiplication}' + '\n'))
+        elif i == '/':
+            if y == 0:
+                print('division by zero is not possible')
+                division = x / y
+                print(division)
+                with open('result.txt', 'a') as file:
+                    file.write(str(f'{x} {i} {y} = {division}' + '\n'))
+        else:
+            print('incorrect operation sign')
 
+        print()
