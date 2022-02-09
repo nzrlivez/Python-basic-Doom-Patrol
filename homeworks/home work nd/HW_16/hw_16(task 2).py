06b5b6b20484575653ef2fb32e9a64db621602e0
Nazar Danyliv <nzrlivez@gmail.com>
2022-02-09 21:45:22 +0200

    HW 16

---
+++ homeworks/home work nd/HW_16/hw_16(task 2).py
@@ -0,0 +1,45 @@
+# 2. Create a script that should find the line that you provided to it in the provided directory.
+
+
+import glob
+import time
+from concurrent.futures import ThreadPoolExecutor
+import psutil
+
+core_num = psutil.cpu_count()
+
+
+def find_by_pattern(filename, pattern):
+    container = []
+    with open(filename) as file:
+        for line in file:
+            if pattern in line:
+                container.append(line)
+    return container
+
+
+def find_all_files(directory_path, pattern):
+    files = glob.glob(f'{directory_path}/*.py')
+    container = []
+    with ThreadPoolExecutor(core_num - 1) as executor:
+        result = list(executor.map(find_by_pattern, files, pattern))
+        container.append(result)
+    return container
+
+
+if __name__ == '__main__':
+    start = time.time()
+    first = find_all_files('', pattern=['ThreadPoolExecutor'])
+    second = find_all_files('', pattern=['ProcessPoolExecutor'])
+    third = find_all_files('', pattern=['get_session'])
+    fourth = find_all_files('', pattern=['result.text'])
+    fifth = find_all_files('../threads_processes', pattern=['ThreadPoolExecutor'])
+    sixth = find_all_files('../threads_processes', pattern=['ProcessPoolExecutor'])
+    seventh = find_all_files('../threads_processes', pattern=['get_session'])
+    eighth = find_all_files('../threads_processes', pattern=['result.text'])
+    print(f'Total time for search: {time.time() - start}')
+    all_search = [first, second, third, fourth, fifth, sixth, seventh, eighth]
+
+    for search in all_search:
+        for element in search:
+            print(element)
